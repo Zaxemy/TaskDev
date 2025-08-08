828a44e8083872b998b66a6324b3a83af1668bda
Zaxemy <dobtana887@gmail.com>
2025-08-09 00:33:16 +0300

    refactor: edit base and task models

--- core/models/tasks.py
+++ core/models/tasks.py
@@ -1,8 +1,10 @@
-from base import Base
+from core.models.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
 class Task(Base):
+    __tablename__ = "tasks"
+
     title: Mapped[str] = mapped_column(String(64), unique=True)
     description: Mapped[str] = mapped_column(String(256))
     is_complete: Mapped[bool] = mapped_column(default=False)
