81b1050d7ffd6b362660c076c3e3656789b4e9e1
Zaxemy <dobtana887@gmail.com>
2025-08-24 12:46:43 +0300

    refactor: users views

--- api/users.py
+++ api/users.py
@@ -16,38 +16,43 @@ from core.Dependencies.auth import get_current_user
 router = APIRouter(prefix="/users")
 
 
-
-
 @router.post("/register/", response_model=schemas.User)
-async def register_user(user: schemas.UserCreate, session: AsyncSession = Depends(db_helper.get_db)):
+async def register_user(
+    user: schemas.UserCreate, session: AsyncSession = Depends(db_helper.get_db)
+):
     """Регистрирует нового пользователя."""
 
-    db_user = await crud_user.get_user_by_username(session=session, username=user.username)
-    
+    db_user = await crud_user.get_user_by_username(
+        session=session, username=user.username
+    )
+
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    
+
     return await crud_user.create_user(session=session, user=user)
 
 
-@router.post("/login", response_model=schemas.Token)
+@router.post("/token/", response_model=schemas.Token)
 async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(db_helper.get_db)
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: AsyncSession = Depends(db_helper.get_db),
 ):
     """Аутентифицирует пользователя и возвращает токен."""
     user = await crud_user.get_user_by_username(session, username=form_data.username)
-    if not user or not security.verify_password(form_data.password, user.hashed_password):
+    if not user or not security.verify_password(
+        form_data.password, user.hashed_password
+    ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    
+
     access_token = security.create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.get("/users/me/", response_model=schemas.User)
+@router.get("/me/", response_model=schemas.User)
 async def read_users_me(current_user: models.User = Depends(get_current_user)):
     """Возвращает данные текущего авторизованного пользователя."""
     return current_user
