0e474b417b5b0d49fd45cf45521f55822b3d6a41
Zaxemy <dobtana887@gmail.com>
2025-09-09 23:21:32 +0300

    create users and access_tokens dbs dependencies

--- core/Dependencies/access_tokens.py
+++ core/Dependencies/access_tokens.py
@@ -3,16 +3,17 @@ from fastapi import Depends, Path, HTTPException
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from core.models.db_helper import db_helper
 from core.models.users import User
-
-
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+from core.models.access_token import AccessToken
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
+    from core.models.access_token import AsyncSession
 
 
-async def get_user_db(
+async def get_access_token_db(
     session: Annotated[
         "AsyncSession",
         Depends(db_helper.get_db),
     ],
 ):
-    yield User.get_db(session=session)
+
+    yield AccessToken.get_db(session=session)
