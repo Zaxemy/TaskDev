bd8f04407e79c41d02d3021bcc75415913dc6396
Zaxemy <dobtana887@gmail.com>
2025-09-09 22:58:09 +0300

    choir: create access token config

--- core/config.py
+++ core/config.py
@@ -2,9 +2,15 @@ from pydantic_settings import BaseSettings
 from pydantic import BaseModel
 from pathlib import Path
 
+
 class DataBaseSettings(BaseModel):
     db_url: str = "sqlite+aiosqlite:///TaskDev.sqlite3"
     echo: bool = True
+
+
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+
 
 class JWTAuth(BaseModel):
     private_key_path: Path = Path("certificates") / "private.pem"
@@ -14,9 +20,8 @@ class JWTAuth(BaseModel):
 
 class Settings(BaseSettings):
     db: DataBaseSettings = DataBaseSettings()
-
+    access_token: AccessToken = AccessToken()
     jwt_auth: JWTAuth = JWTAuth()
-
 
 
 settings = Settings()
