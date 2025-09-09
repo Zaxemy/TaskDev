8ccdb199e55bec4e427aa9b9f6c8600f1fd38925
Zaxemy <dobtana887@gmail.com>
2025-09-09 22:21:57 +0300

    feat: create AccessToken model

---
+++ core/models/access_token.py
@@ -0,0 +1,26 @@
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
+from core.models.base import Base
+
+from core.types.user_id import UserIdType
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTable[UserIdType], Base):
+
+    user_id: Mapped[UserIdType] = mapped_column(
+        Integer, ForeignKey("users.id", ondelete="cascade"), nullable=False
+    )
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyAccessTokenDatabase(session=session, user_table=cls)
--- core/models/users.py
+++ core/models/users.py
@@ -1,16 +1,19 @@
 from .base import Base
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer
 from typing import TYPE_CHECKING
+
+from core.types.user_id import UserIdType
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-
-class User(SQLAlchemyBaseUserTable[int], Base):
+class User(SQLAlchemyBaseUserTable[UserIdType], Base):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session=session, user_table=User)
+        return SQLAlchemyUserDatabase(session=session, user_table=cls)
