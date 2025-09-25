1259f229845178cf41d4a126b6b7947e2c7b883c
Zaxemy <dobtana887@gmail.com>
2025-09-25 21:57:48 +0300

    add: schemas for user

--- core/models/users.py
+++ core/models/users.py
@@ -1,19 +1,29 @@
-from .base import Base
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from core.models.base import Base
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer
-from typing import TYPE_CHECKING
-
+from sqlalchemy import String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from core.types.user_id import UserIdType
-
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(SQLAlchemyBaseUserTable[UserIdType], Base):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    @classmethod
-    def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session=session, user_table=cls)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String(1024))
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    is_verified: Mapped[bool] = mapped_column(default=False)
+
+    first_name: Mapped[str | None] = mapped_column(String(25))
+    last_name: Mapped[str | None] = mapped_column(String(25))
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[DateTime | None] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
