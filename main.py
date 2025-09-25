7b8d65d15ff393875f45b56dd924169797281d8d
Zaxemy <dobtana887@gmail.com>
2025-09-25 22:08:25 +0300

    feat: routers for authentication, registration, logout....

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
 import uvicorn
 from api.tasks import router as tasks_router
-from api.users import router as users_router
+from core.Dependencies.auth import fastapi_users
+from core.authentication.auth_backend import auth_backend
+from core.schemas import users as user_schemas
+
 
 app = FastAPI()
 
 app.include_router(tasks_router)
 
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+
+app.include_router(
+    fastapi_users.get_register_router(user_schemas.UserRead, user_schemas.UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_users_router(user_schemas.UserRead, user_schemas.UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
 
 if __name__ == "__main__":
