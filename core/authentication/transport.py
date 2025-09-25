fcce324d01d86471ed52f4095acef5c4ac8ded2f
Zaxemy <dobtana887@gmail.com>
2025-09-25 22:00:21 +0300

    add(fastapi users): transport cookie

--- core/authentication/transport.py
+++ core/authentication/transport.py
@@ -1,6 +1,29 @@
-from fastapi_users.authentication import BearerTransport
+from fastapi import Response
+from fastapi_users.authentication import CookieTransport
+from core.config import settings
 
 
-bearer_transport = BearerTransport(
-    tokenUrl="auth/jwt/login",
+class CustomCookieTransport(CookieTransport):
+    """
+    Кастомный транспорт для Cookie, httponly, samesite, 
+    """
+    async def get_login_response(self, token: str) -> Response:
+        response = Response()
+        response.set_cookie(
+            key=self.cookie_name,
+            value=token,
+            max_age=self.cookie_max_age,
+            path=self.cookie_path,
+            domain=self.cookie_domain,
+            secure=self.cookie_secure,
+            httponly=True,  
+            samesite="lax",
+        )
+        return response
+
+# Настройка Cookie транспорта
+cookie_transport = CustomCookieTransport(
+    cookie_name="auth_token",
+    cookie_max_age=settings.jwt_auth.LIFETIME_SECONDS,
+    cookie_secure=False, 
 )
