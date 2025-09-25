e1603ec07dd8c795dd4d7426b95fc9923801e506
Zaxemy <dobtana887@gmail.com>
2025-09-25 22:03:04 +0300

    add: dependency fastapi users

---
+++ core/Dependencies/auth.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from fastapi_users import FastAPIUsers
+
+from core.Dependencies.users import get_user_db
+from core.models.users import User
+from core.authentication.auth_backend import auth_backend
+from core.authentication.manage_auth import UserManager
+from core.authentication.manage_auth import get_user_manager
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
